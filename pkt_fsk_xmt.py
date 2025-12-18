fac3575502d558d43970a6e9f93cb43a2169007d
mpalldritt <mpalldritt@gmail.com>
2025-12-18 19:56:36 +1100

    FSK modulation protocol
    Software used for data transfer during my PHD research and development

---
+++ pkt_fsk_xmt.py
@@ -0,0 +1,349 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+#
+# SPDX-License-Identifier: GPL-3.0
+#
+# GNU Radio Python Flow Graph
+# Title: pkt_fsk_xmt
+# Author: Barry Duggan modified by MA
+# Description: packet FSK xmt
+# GNU Radio version: 3.10.10.0
+
+from PyQt5 import Qt
+from gnuradio import qtgui
+from gnuradio import blocks
+import pmt
+from gnuradio import digital
+from gnuradio import gr
+from gnuradio.filter import firdes
+from gnuradio.fft import window
+import sys
+import signal
+from PyQt5 import Qt
+from argparse import ArgumentParser
+from gnuradio.eng_arg import eng_float, intx
+from gnuradio import eng_notation
+from gnuradio import zeromq
+import math
+import sip
+
+
+
+class pkt_fsk_xmt(gr.top_block, Qt.QWidget):
+
+    def __init__(self, InFile='default'):
+        gr.top_block.__init__(self, "pkt_fsk_xmt", catch_exceptions=True)
+        Qt.QWidget.__init__(self)
+        self.setWindowTitle("pkt_fsk_xmt")
+        qtgui.util.check_set_qss()
+        try:
+            self.setWindowIcon(Qt.QIcon.fromTheme('gnuradio-grc'))
+        except BaseException as exc:
+            print(f"Qt GUI: Could not set Icon: {str(exc)}", file=sys.stderr)
+        self.top_scroll_layout = Qt.QVBoxLayout()
+        self.setLayout(self.top_scroll_layout)
+        self.top_scroll = Qt.QScrollArea()
+        self.top_scroll.setFrameStyle(Qt.QFrame.NoFrame)
+        self.top_scroll_layout.addWidget(self.top_scroll)
+        self.top_scroll.setWidgetResizable(True)
+        self.top_widget = Qt.QWidget()
+        self.top_scroll.setWidget(self.top_widget)
+        self.top_layout = Qt.QVBoxLayout(self.top_widget)
+        self.top_grid_layout = Qt.QGridLayout()
+        self.top_layout.addLayout(self.top_grid_layout)
+
+        self.settings = Qt.QSettings("GNU Radio", "pkt_fsk_xmt")
+
+        try:
+            geometry = self.settings.value("geometry")
+            if geometry:
+                self.restoreGeometry(geometry)
+        except BaseException as exc:
+            print(f"Qt GUI: Could not restore geometry: {str(exc)}", file=sys.stderr)
+
+        ##################################################
+        # Parameters
+        ##################################################
+        self.InFile = InFile
+
+        ##################################################
+        # Variables
+        ##################################################
+        self.space = space = 39500
+        self.mark = mark = 40500
+        self.fsk_deviation = fsk_deviation = (abs)(mark-space)
+        self.center = center = (mark+space)/2
+        self.vco_max = vco_max = center+fsk_deviation
+        self.vco_offset = vco_offset = space/vco_max
+        self.samp_rate = samp_rate = 192000
+        self.baud = baud = 1200
+        self.access_key = access_key = '11100001010110101110100010010011'
+        self.thresh = thresh = 1
+        self.repeat = repeat = (int)(samp_rate/baud)
+        self.inp_amp = inp_amp = (mark/vco_max)-vco_offset
+        self.hdr_format = hdr_format = digital.header_format_default(access_key, 0)
+
+        ##################################################
+        # Blocks
+        ##################################################
+
+        self.zeromq_pub_sink_0 = zeromq.pub_sink(gr.sizeof_gr_complex, 1, 'tcp://127.0.0.1:49600', 100, False, (-1), '', True, True)
+        self.qtgui_time_sink_x_0 = qtgui.time_sink_f(
+            8192, #size
+            samp_rate, #samp_rate
+            'Transmit data', #name
+            1, #number of inputs
+            None # parent
+        )
+        self.qtgui_time_sink_x_0.set_update_time(0.10)
+        self.qtgui_time_sink_x_0.set_y_axis(-0.1, 1.1)
+
+        self.qtgui_time_sink_x_0.set_y_label('Amplitude', "")
+
+        self.qtgui_time_sink_x_0.enable_tags(True)
+        self.qtgui_time_sink_x_0.set_trigger_mode(qtgui.TRIG_MODE_NORM, qtgui.TRIG_SLOPE_POS, 0.1, 0.0, 0, "packet_len")
+        self.qtgui_time_sink_x_0.enable_autoscale(True)
+        self.qtgui_time_sink_x_0.enable_grid(False)
+        self.qtgui_time_sink_x_0.enable_axis_labels(True)
+        self.qtgui_time_sink_x_0.enable_control_panel(False)
+        self.qtgui_time_sink_x_0.enable_stem_plot(False)
+
+
+        labels = ['', '', '', '', '',
+            '', '', '', '', '']
+        widths = [1, 1, 1, 1, 1,
+            1, 1, 1, 1, 1]
+        colors = ['blue', 'red', 'green', 'black', 'cyan',
+            'magenta', 'yellow', 'dark red', 'dark green', 'dark blue']
+        alphas = [1.0, 1.0, 1.0, 1.0, 1.0,
+            1.0, 1.0, 1.0, 1.0, 1.0]
+        styles = [1, 1, 1, 1, 1,
+            1, 1, 1, 1, 1]
+        markers = [-1, -1, -1, -1, -1,
+            -1, -1, -1, -1, -1]
+
+
+        for i in range(1):
+            if len(labels[i]) == 0:
+                self.qtgui_time_sink_x_0.set_line_label(i, "Data {0}".format(i))
+            else:
+                self.qtgui_time_sink_x_0.set_line_label(i, labels[i])
+            self.qtgui_time_sink_x_0.set_line_width(i, widths[i])
+            self.qtgui_time_sink_x_0.set_line_color(i, colors[i])
+            self.qtgui_time_sink_x_0.set_line_style(i, styles[i])
+            self.qtgui_time_sink_x_0.set_line_marker(i, markers[i])
+            self.qtgui_time_sink_x_0.set_line_alpha(i, alphas[i])
+
+        self._qtgui_time_sink_x_0_win = sip.wrapinstance(self.qtgui_time_sink_x_0.qwidget(), Qt.QWidget)
+        self.top_grid_layout.addWidget(self._qtgui_time_sink_x_0_win, 1, 0, 1, 3)
+        for r in range(1, 2):
+            self.top_grid_layout.setRowStretch(r, 1)
+        for c in range(0, 3):
+            self.top_grid_layout.setColumnStretch(c, 1)
+        self.qtgui_sink_x_0 = qtgui.sink_c(
+            1024, #fftsize
+            window.WIN_BLACKMAN_hARRIS, #wintype
+            0, #fc
+            samp_rate, #bw
+            "output to ZMQ sockets", #name
+            True, #plotfreq
+            True, #plotwaterfall
+            True, #plottime
+            True, #plotconst
+            None # parent
+        )
+        self.qtgui_sink_x_0.set_update_time(1.0/10)
+        self._qtgui_sink_x_0_win = sip.wrapinstance(self.qtgui_sink_x_0.qwidget(), Qt.QWidget)
+
+        self.qtgui_sink_x_0.enable_rf_freq(False)
+
+        self.top_layout.addWidget(self._qtgui_sink_x_0_win)
+        self.digital_protocol_formatter_bb_0_0 = digital.protocol_formatter_bb(hdr_format, "packet_len")
+        self.digital_crc32_bb_0_0 = digital.crc32_bb(False, "packet_len", True)
+        self.blocks_vco_c_0 = blocks.vco_c(samp_rate, (2*math.pi*vco_max), 1.0)
+        self.blocks_uchar_to_float_0 = blocks.uchar_to_float()
+        self.blocks_throttle2_0_0 = blocks.throttle( gr.sizeof_char*1, samp_rate, True, 0 if "auto" == "auto" else max( int(float(0.1) * samp_rate) if "auto" == "time" else int(0.1), 1) )
+        self.blocks_tagged_stream_mux_0_0 = blocks.tagged_stream_mux(gr.sizeof_char*1, 'packet_len', 0)
+        self.blocks_stream_to_tagged_stream_1 = blocks.stream_to_tagged_stream(gr.sizeof_char, 1, 75, "packet_len")
+        self.blocks_repeat_0 = blocks.repeat(gr.sizeof_char*1, repeat)
+        self.blocks_repack_bits_bb_1_0 = blocks.repack_bits_bb(8, 1, '', False, gr.GR_MSB_FIRST)
+        self.blocks_multiply_const_vxx_0 = blocks.multiply_const_ff(inp_amp)
+        self.blocks_file_source_0 = blocks.file_source(gr.sizeof_char*1, 'C:\\Users\\Michael Alldritt\\Desktop\\Citytech Project 2024\\2023 GRC Project Work Tracked & Updated\\Text and target files\\The-Iliad.txt', False, 0, 0)
+        self.blocks_file_source_0.set_begin_tag(pmt.PMT_NIL)
+        self.blocks_add_const_vxx_0 = blocks.add_const_ff(vco_offset)
+
+
+        ##################################################
+        # Connections
+        ##################################################
+        self.connect((self.blocks_add_const_vxx_0, 0), (self.blocks_vco_c_0, 0))
+        self.connect((self.blocks_file_source_0, 0), (self.blocks_stream_to_tagged_stream_1, 0))
+        self.connect((self.blocks_multiply_const_vxx_0, 0), (self.blocks_add_const_vxx_0, 0))
+        self.connect((self.blocks_repack_bits_bb_1_0, 0), (self.blocks_repeat_0, 0))
+        self.connect((self.blocks_repeat_0, 0), (self.blocks_uchar_to_float_0, 0))
+        self.connect((self.blocks_stream_to_tagged_stream_1, 0), (self.blocks_throttle2_0_0, 0))
+        self.connect((self.blocks_tagged_stream_mux_0_0, 0), (self.blocks_repack_bits_bb_1_0, 0))
+        self.connect((self.blocks_throttle2_0_0, 0), (self.digital_crc32_bb_0_0, 0))
+        self.connect((self.blocks_uchar_to_float_0, 0), (self.blocks_multiply_const_vxx_0, 0))
+        self.connect((self.blocks_uchar_to_float_0, 0), (self.qtgui_time_sink_x_0, 0))
+        self.connect((self.blocks_vco_c_0, 0), (self.qtgui_sink_x_0, 0))
+        self.connect((self.blocks_vco_c_0, 0), (self.zeromq_pub_sink_0, 0))
+        self.connect((self.digital_crc32_bb_0_0, 0), (self.blocks_tagged_stream_mux_0_0, 1))
+        self.connect((self.digital_crc32_bb_0_0, 0), (self.digital_protocol_formatter_bb_0_0, 0))
+        self.connect((self.digital_protocol_formatter_bb_0_0, 0), (self.blocks_tagged_stream_mux_0_0, 0))
+
+
+    def closeEvent(self, event):
+        self.settings = Qt.QSettings("GNU Radio", "pkt_fsk_xmt")
+        self.settings.setValue("geometry", self.saveGeometry())
+        self.stop()
+        self.wait()
+
+        event.accept()
+
+    def get_InFile(self):
+        return self.InFile
+
+    def set_InFile(self, InFile):
+        self.InFile = InFile
+
+    def get_space(self):
+        return self.space
+
+    def set_space(self, space):
+        self.space = space
+        self.set_center((self.mark+self.space)/2)
+        self.set_fsk_deviation((abs)(self.mark-self.space))
+        self.set_vco_offset(self.space/self.vco_max)
+
+    def get_mark(self):
+        return self.mark
+
+    def set_mark(self, mark):
+        self.mark = mark
+        self.set_center((self.mark+self.space)/2)
+        self.set_fsk_deviation((abs)(self.mark-self.space))
+        self.set_inp_amp((self.mark/self.vco_max)-self.vco_offset)
+
+    def get_fsk_deviation(self):
+        return self.fsk_deviation
+
+    def set_fsk_deviation(self, fsk_deviation):
+        self.fsk_deviation = fsk_deviation
+        self.set_vco_max(self.center+self.fsk_deviation)
+
+    def get_center(self):
+        return self.center
+
+    def set_center(self, center):
+        self.center = center
+        self.set_vco_max(self.center+self.fsk_deviation)
+
+    def get_vco_max(self):
+        return self.vco_max
+
+    def set_vco_max(self, vco_max):
+        self.vco_max = vco_max
+        self.set_inp_amp((self.mark/self.vco_max)-self.vco_offset)
+        self.set_vco_offset(self.space/self.vco_max)
+
+    def get_vco_offset(self):
+        return self.vco_offset
+
+    def set_vco_offset(self, vco_offset):
+        self.vco_offset = vco_offset
+        self.set_inp_amp((self.mark/self.vco_max)-self.vco_offset)
+        self.blocks_add_const_vxx_0.set_k(self.vco_offset)
+
+    def get_samp_rate(self):
+        return self.samp_rate
+
+    def set_samp_rate(self, samp_rate):
+        self.samp_rate = samp_rate
+        self.set_repeat((int)(self.samp_rate/self.baud))
+        self.blocks_throttle2_0_0.set_sample_rate(self.samp_rate)
+        self.qtgui_sink_x_0.set_frequency_range(0, self.samp_rate)
+        self.qtgui_time_sink_x_0.set_samp_rate(self.samp_rate)
+
+    def get_baud(self):
+        return self.baud
+
+    def set_baud(self, baud):
+        self.baud = baud
+        self.set_repeat((int)(self.samp_rate/self.baud))
+
+    def get_access_key(self):
+        return self.access_key
+
+    def set_access_key(self, access_key):
+        self.access_key = access_key
+        self.set_hdr_format(digital.header_format_default(self.access_key, 0))
+
+    def get_thresh(self):
+        return self.thresh
+
+    def set_thresh(self, thresh):
+        self.thresh = thresh
+
+    def get_repeat(self):
+        return self.repeat
+
+    def set_repeat(self, repeat):
+        self.repeat = repeat
+        self.blocks_repeat_0.set_interpolation(self.repeat)
+
+    def get_inp_amp(self):
+        return self.inp_amp
+
+    def set_inp_amp(self, inp_amp):
+        self.inp_amp = inp_amp
+        self.blocks_multiply_const_vxx_0.set_k(self.inp_amp)
+
+    def get_hdr_format(self):
+        return self.hdr_format
+
+    def set_hdr_format(self, hdr_format):
+        self.hdr_format = hdr_format
+        self.digital_protocol_formatter_bb_0_0.set_header_format(self.hdr_format)
+
+
+
+def argument_parser():
+    description = 'packet FSK xmt'
+    parser = ArgumentParser(description=description)
+    parser.add_argument(
+        "--InFile", dest="InFile", type=str, default='default',
+        help="Set File Name [default=%(default)r]")
+    return parser
+
+
+def main(top_block_cls=pkt_fsk_xmt, options=None):
+    if options is None:
+        options = argument_parser().parse_args()
+
+    qapp = Qt.QApplication(sys.argv)
+
+    tb = top_block_cls(InFile=options.InFile)
+
+    tb.start()
+
+    tb.show()
+
+    def sig_handler(sig=None, frame=None):
+        tb.stop()
+        tb.wait()
+
+        Qt.QApplication.quit()
+
+    signal.signal(signal.SIGINT, sig_handler)
+    signal.signal(signal.SIGTERM, sig_handler)
+
+    timer = Qt.QTimer()
+    timer.start(500)
+    timer.timeout.connect(lambda: None)
+
+    qapp.exec_()
+
+if __name__ == '__main__':
+    main()
